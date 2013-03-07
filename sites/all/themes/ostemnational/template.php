cd4a9ec81b7796e74ddbe91d059021cf342808d8
Adam Stoffel <adam@cidesigninc.com>
2013-03-07 16:13:05 -0600

    Styles for connector buttons and tabs.

--- sites/all/themes/ostemnational/template.php
+++ sites/all/themes/ostemnational/template.php
@@ -24,3 +24,34 @@ function getPageID($is_front) {
 function getBodyID($is_front = false) {
 	return 'page-' . getPageID($is_front);
 }
+
+function ostemnational_form_alter(&$form, &$form_state, $form_id) {
+	if($form_id == 'connector_button_form') {
+		foreach($form as $key => $form_child) {
+			if($i == 1)  $form[$key]['#attributes']['class'][] = 'first';
+			if(array_key_exists('connector', $form_child)) $form[$key]['#value'] = 'Login with ' . $form_child['connector']['#value']['title'];
+		}
+	}
+}
+
+function ostemnational_connector_buttons($variables) {
+
+  $form = $variables['form'];
+  if (!$form['#has_buttons']) return NULL;
+  $output = '';
+  $buttons = array();
+  $i == 0; error_log('custom connector buttons');
+  foreach (element_children($form) as $key) {
+    if ($form[$key]['#type'] == 'submit') {
+      $i++;
+      $class = str_replace('_', '-', $key);
+      $form[$key]['#attributes']['class'][] = 'connector-button';
+      $form[$key]['#attributes']['class'][] = $class;
+      if($i == 1) $class .= ' first';
+      $form[$key]['#prefix'] = '<span class="connector-button-wrapper ' .$class . '">';
+      $form[$key]['#suffix'] = '</span>';
+    }
+  }
+  return drupal_render_children($form);
+
+}

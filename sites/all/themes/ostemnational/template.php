ae3f7a248f0d265a6a3e1e47c489b6d0ed4f0476
Adam Stoffel <adamstoffel@gmail.com>
2014-03-18 00:28:50 -0400

    Azure ACS module and styles.

--- sites/all/themes/ostemnational/template.php
+++ sites/all/themes/ostemnational/template.php
@@ -25,82 +25,87 @@ function getBodyID($is_front = false) {
 	return 'page-' . getPageID($is_front);
 }
 
+function ostemnational_block_list_alter(&$blocks) {
+    if(!drupal_match_path(drupal_get_path_alias(current_path()), 'user/register')) return;
+    if (!isset($_SESSION) || empty($_SESSION['azure_acs_data'])) return;
+    foreach($blocks as $bid => $block) {
+        if($block->module == "azure_acs") unset($blocks[$bid]);      
+    }
+}
+
 function ostemnational_form_alter(&$form, &$form_state, $form_id) {
-	if($form_id == 'connector_button_form') {
-		foreach($form as $key => $form_child) {
-			if(is_array($form_child) && array_key_exists('connector', $form_child)) {
-				$form[$key]['#value'] = $form_child['connector']['#value']['title'];
-			}
-		}
-	}
-}
+    
+    if (isset($_SESSION) && !empty($_SESSION['azure_acs_data'])) {
+        
+        $mail = isset($_SESSION['azure_acs_data']['emailaddress']) ? $_SESSION['azure_acs_data']['emailaddress'] : '';
+        $fname = isset($_SESSION['azure_acs_data']['givenname']) ? $_SESSION['azure_acs_data']['givenname'] : '';
+        $lname = isset($_SESSION['azure_acs_data']['surname']) ? $_SESSION['azure_acs_data']['surname'] : '';
+        $name = isset($_SESSION['azure_acs_data']['name']) ? $_SESSION['azure_acs_data']['name'] :
+            (empty($fname) || empty($lname) ? '' : $fname . ' ' . $lname);
+        $user = empty($mail) ? $name : explode('@', $mail)[0];
+        
+        if(!empty($name)) {
+            $explode = explode(" ", $name);
+            if(empty($fname)) $fname = $explode[0];
+            if(empty($lname) && count($explode) > 1) $lname = $explode[count($explode)-1];
+        }
+        
+        if(strpos($_SESSION['azure_acs_data']['identityprovider'], 'Facebook') !== FALSE
+            && isset($_SESSION['azure_acs_data']['AccessToken'])) {
+            $url = "https://graph.facebook.com/" . $_SESSION['azure_acs_data']['nameidentifier']
+                . "?access_token=" . $_SESSION['azure_acs_data']['AccessToken'];
+            $url = 'https://rest.db.ripe.net/ripe/inetnum/192.168.0.1.json';
+            $ch = curl_init();
+            $timeout = 500;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            $data = json_decode(curl_exec($ch));
+            curl_close($ch);
+            
+            if(!empty($data)) {
+                $bday = isset($data->birthday) ? $data->birthday : '';
+                $gender = isset($data->gender) ? $data->gender : '';
+                $tz = isset($data->timezone) ? $data->timezone : '';
+            }
+        }
 
-function ostemnational_connector_buttons($variables) {
+        if (isset($form['account'])) {
+            
+            if(isset($form['account']['pass'])) {
+                $form['account']['pass']['#type'] = 'hidden';
+                $form['account']['pass']['#value'] = user_password();
+            }
+            
+            if (isset($form['account']['name'])) {
+                $form['account']['name']['#default_value'] = $user;
+            }
+            if (isset($form['account']['mail'])) {
+                $form['account']['mail']['#default_value'] = $mail;
+            }
 
-  $form = $variables['form'];
-  if (!$form['#has_buttons']) return NULL;
-  $output = '';
-  $buttons = array();
-  $i = 0;
-  foreach (element_children($form) as $key) {
-    if ($form[$key]['#type'] == 'submit') {
-      $i++;
-      $class = str_replace('_', '-', $key);
-      $form[$key]['#attributes']['class'][] = 'connector-button';
-      $form[$key]['#attributes']['class'][] = $class;
-      if($i == 1) $class .= ' first';
-      $form[$key]['#prefix'] = '<span class="connector-button-wrapper ' .$class . '">';
-      $form[$key]['#suffix'] = '</span>';
+            if (isset($form['field_first_name']['#language']) &&
+                isset($form['field_first_name'][($form['field_first_name']['#language'])][0]['value'])) {
+                $form['field_first_name'][($form['field_first_name']['#language'])][0]['value']['#default_value'] = $fname;
+            }
+            if (isset($form['field_last_name']['#language']) &&
+                isset($form['field_last_name'][($form['field_last_name']['#language'])][0]['value'])) {
+                $form['field_last_name'][($form['field_last_name']['#language'])][0]['value']['#default_value'] = $lname;
+            }
+            if (isset($form['field_name']['#language']) &&
+                isset($form['field_name'][($form['field_name']['#language'])][0]['value'])) {
+                $form['field_name'][($form['field_name']['#language'])][0]['value']['#default_value'] = $name;
+            }
+            if (!empty($bday) && isset($form['field_date_of_birth']['#language']) &&
+                isset($form['field_date_of_birth'][($form['field_date_of_birth']['#language'])][0]['value'])) {
+                $form['field_date_of_birth'][($form['field_date_of_birth']['#language'])][0]['value']['#default_value'] = $bday;
+            }
+        }
+        
+        $form['azure_acs_data'] = array(
+          '#type' => 'value',
+          '#value' => $_SESSION['azure_acs_data'],
+        );
     }
-  }
-  return drupal_render_children($form);
-
-}
-
-function ostemnational_connector_connections_list_tableselect($variables) {
-  $form = $variables['form'];
-  $header = array('');
-
-  foreach (element_children($form['header']) as $key) {
-    if($key != 'cid') $header[] = drupal_render($form['header'][$key]);
-  }
-
-  $rows = array();
-  $number_of_rows = 0;
-  if (!empty($form['connector'])) {
-    foreach (element_children($form['connector']) as $key) {
-      $row = array();
-      $row[] = drupal_render($form['primary'][$key]);
-      $row[] = drupal_render($form['connector'][$key]);
-      $row[] = drupal_render($form['operations'][$key]);
-      $rows[] = $row;
-      $number_of_rows++;
-    }
-
-    if ($number_of_rows == 1) {
-      unset($rows[0][0]);
-      unset($header[0]);
-    }
-  }
-  else {
-    $rows[] = array(array(
-        'data' => t('No connections available.'),
-        'colspan' => '6',
-      ));
-  }
-
-  $output = theme('table', array('header' => $header, 'rows' => $rows));
-  if ($number_of_rows < 2) {
-    unset($form['actions']);
-  }
-
-  return $output;
-}
-
-function ostemnational_menu_local_tasks_alter(&$data, $router_item, $root_path) {
-	if(count($data['tabs']) > 0) {
-		foreach($data['tabs'][0]['output'] as $key => $item) {
-			if($item['#link']['path'] == 'user/%/connections') $data['tabs'][0]['output'][$key]['#link']['title'] = 'Social Media Connections';
-		}
-	}
 }
